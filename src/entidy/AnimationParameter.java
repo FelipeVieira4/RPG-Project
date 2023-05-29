a1b069dca26778ef8efc2b78ab92e13e470093b1
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2023-05-29 08:55:05 -0300

    working on animation system

--- src/entidy/AnimationParameter.java
+++ src/entidy/AnimationParameter.java
@@ -2,21 +2,12 @@ package entidy;
 
 public class AnimationParameter {
 	private byte row,coll;
-	private byte FStart,FEnd; //frame start and end
+	private byte numberFrames; //frame start and end
 	
-	public AnimationParameter(byte row,byte col,byte start,byte end) {
+	public AnimationParameter(byte row,byte col,byte Frames) {
 		this.row=row;
 		this.coll=col;
-		this.FStart=start;
-		this.FEnd=end;
-	}
-
-	public byte getFStart() {
-		return FStart;
-	}
-
-	public byte getFEnd() {
-		return FEnd;
+		this.numberFrames=Frames;
 	}
 	
 	

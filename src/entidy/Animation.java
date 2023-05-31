fd4357d97abedc973daf3fed0b7cbf0d0975e5d9
Felipe Vieira <101891565+FelipeVieira4@users.noreply.github.com>
2023-05-31 19:00:35 -0300

    Working on anomation system

--- src/entidy/Animation.java
+++ src/entidy/Animation.java
@@ -5,11 +5,16 @@ public class Animation{
 
 	int posFrameX,posFrameY;
 	int frameNumber;
+	int frameNow=0;
 	
 	public Animation(int x,int y,int num) {
 		this.posFrameX=x;
 		this.posFrameY=y;
 		this.frameNumber=num;
+	}
+	
+	public void updateAnim() {
+		
 	}
 
 }
